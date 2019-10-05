65054970b1b70d1384c22018fd9502bad3d36563
Marc Bourgoin <themard85@gmail.com>
2019-10-05 09:32:51 -0600

    deen: use prebuilt update_engine_sideload
    * source built was failing with...
    
    [0915/001809.730118:INFO:postinstall_runner_action.cc(130)] Found previously mounted filesystem at /postinstall
    [0915/001809.732465:INFO:postinstall_runner_action.cc(175)] /dev/block/bootdevice/by-name/system_a has been mounted R/W 5 times.
    [0915/001809.732488:INFO:postinstall_runner_action.cc(179)] Running backuptool scripts
    [0915/001809.732958:ERROR:postinstall_runner_action.cc(185)] Failed to set backuptool context
    [0915/001809.732976:ERROR:postinstall_runner_action.cc(409)] Postinstall action failed.
    [0915/001809.732996:INFO:action_processor.cc(116)] ActionProcessor: finished last action PostinstallRunnerAction with code ErrorCode::kPostinstallRunnerError
    
    Change-Id: I49b69a5b79e9e9c3ce341af69bdd0ba020355630

--- device.mk
+++ device.mk
@@ -29,7 +29,6 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
-    update_engine_sideload \
     update_verifier
 
 # The following modules are included in debuggable builds only.
